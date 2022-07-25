83786d0a7b2dbcac0427b514fb179c8bcf040e30
Ramel Henderson <boomboom3k@gmail.com>
2022-07-25 04:44:53 -0400

    made another function to handle interface slices

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 // CSV2XLSX Returns a type xlsx.File -- Save file once completed. Thanks -> "github.com/tealeg/xlsx"
-func CSV2XLSX(indexSheetName string, dataSet [][]interface{}) *xlsx.File {
+func CSV2XLSX(indexSheetName string, dataSet [][]string) *xlsx.File {
 	xlsxFile := xlsx.NewFile()
 	sheet, err := xlsxFile.AddSheet(indexSheetName)
 	if err != nil {
@@ -17,8 +17,21 @@ func CSV2XLSX(indexSheetName string, dataSet [][]interface{}) *xlsx.File {
 		thisRow := sheet.AddRow()
 		for _, data := range datum {
 			cell := thisRow.AddCell()
-			cell.Value = data.(string)
+			cell.Value = data
 		}
 	}
 	return xlsxFile
+}
+
+//CSV2XLSX_RAW - Handles interface slices for CSV2XLSX
+func CSV2XLSX_RAW(indexSheetName string, dataSet [][]interface{}) *xlsx.File {
+	var asStringSlice [][]string
+	for _, columnData := range dataSet {
+		var data []string
+		for _, datum := range columnData {
+			data = append(data, datum.(string))
+		}
+		asStringSlice = append(asStringSlice, data)
+	}
+	return CSV2XLSX(indexSheetName, asStringSlice)
 }
